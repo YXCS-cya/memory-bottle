d6fa80b2e08e640f34c40ab7977c87b79fccaa32
YXCS-cya <162129534+YXCS-cya@users.noreply.github.com>
2025-06-22 20:47:26 +0800

    Initial Spring Boot project

---
+++ src/main/java/com/memorybottle/memory_app/MemoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.memorybottle.memory_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memorybottle/memory_app/MemoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorybottle.memory_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
